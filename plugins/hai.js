7f9c2c70ebc875094f40b5cfa90cb0575985cbe8
Jarotr <101513858+Jarotr@users.noreply.github.com>
2022-05-26 23:59:12 +0700

    Update hai.js

--- plugins/hai.js
+++ plugins/hai.js
@@ -2,12 +2,12 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
   let ext= `
   Status : *_Online_* 
-*📮: HAI SAYA ZIFABOTZ SILAHKAN KLIK BUTTON DI BAWAH!*
+*📮: HAI SAYA JAROTBOTZ SILAHKAN KLIK BUTTON DI BAWAH!*
 `.trim()
 conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'uy apaan')).buffer(), ext, 'bot online', 'COMMAND', '.command', 'OWNER', '.owner2', m)
 
 }
-handler.customPrefix = /^(zifa|zifabotz|zif|menu|hai|hallo|hii|hello|cuy)$/i
+handler.customPrefix = /^(oscar|botjr|rot|menu|hai|hallo|hii|hello|cuy)$/i
 handler.command = new RegExp
 
 module.exports = handler
